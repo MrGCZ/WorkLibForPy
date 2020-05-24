ca5e666bed25653e5fcd6f174354fdcbeaf4fbce
Chengzhe Gong <34358056+MrGCZ@users.noreply.github.com>
2020-05-25 00:16:40 +0800

    525

--- JdbcUtils.py
+++ JdbcUtils.py
@@ -1,5 +1,7 @@
 #encoding=utf-8
 import pymysql
+from WindPy import w
+
 
 class jdbc_connect:
 
@@ -64,17 +66,45 @@ class jdbc_connect:
         except BaseException:
             print("db close error")
 
-    def dickey_convert2_dbkey(self, converted_key, convert_type):
-        query_sql = "select convert2_key from etl_convert_dictkey_mapping t where t.convert_type=\'" + convert_type + \
-                    "\' and t.converted_key=\'" + converted_key + "\'"
-        # print(query_sql)
-        convert2_type = self.select(query_sql)[0][0]
-        return convert2_type
+    def dictkey_to_dbkey(self, converted_key, convert_type):
+        try:
+            query_sql = "select convert2_key from etl_convert_dictkey_mapping t where t.convert_type=\'" + convert_type + \
+                        "\' and t.converted_key=\'" + converted_key + "\'"
+            # print(query_sql)
+            convert2_type = self.select(query_sql)[0][0]
+            return convert2_type
+        except:
+            return converted_key
+
+    # convert key of dict to bdkey, return converted dict
+    def dict_to_dbdict(self, dict, convert_type):
+        rdict = {}
+        for key in dict.keys():
+            rdict[self.dictkey_to_dbkey(key, convert_type)] = dict[key]
+        return rdict
 
 
 if __name__ == "__main__":
     connect1 = jdbc_connect("localhost", "root", "1026", "ia2")
-    connect1.dickey_convert2_dbkey("windcode", "sec_info")
+    # connect1.dickey_convert2_dbkey("windcode", "sec_info")
+
+
+
+    w.start()  # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒
+    w.isconnected()  # 判断WindPy是否已经登录成功
+    dict1 = {}
+    # wind api
+    b = w.wss("000001.OF,000005.OF,000009.OF",
+              "trade_code,windcode,sec_name,sec_englishname,exchange_cn,ipo_date,isin_code,fund_fullname,name_official")
+    print(b)
+
+    # convert to dict
+    for i in range(len(b.Fields)):
+        dict1[b.Fields[i]] = b.Data[i]
+
+    print(dict1)
+
+    print(connect1.dict_to_dbdict(dict1, "sec_info"))
 
     # print(connect1)
     # rlist = connect1.select("select * from t_book")
